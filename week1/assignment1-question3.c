f9697df243b75db4a8bd63b6d42d0aa7f64e58d3
meetraojatin <124671041+meetraojatin@users.noreply.github.com>
2023-02-28 12:06:26 +0530

    Create assignment1-question3.c

---
+++ week1/assignment1-question3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+  int a,b,c;
+  
+  scanf("%d %d %d",&a,&b,&c);
+  
+  if(a>b && b>c){
+    printf("1");
+  }else if(c>b && b>a){
+    printf("1");
+  }else{
+    printf("0");
+  }
+  
+  return 0;
+}
