134aee98d7f6be0078a9e8349c512952a8083198
meetraojatin <124671041+meetraojatin@users.noreply.github.com>
2023-02-28 12:02:23 +0530

    Update assignment2-question1.c

--- week2/assignment2-question1.c
+++ week2/assignment2-question1.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+  int num, num1, num2;
+  scanf("%d", &num);
+  num1 = num;
+  num2 = 0;
+  while(num!=-1) {
+    scanf("%d", &num);
+    if(num1 < num) {
+      num2 = num1;
+      num1 = num;
+    }
+    if(num2 < num && num1 != num){
+      num2=num;
+    }
+  }
+  if(num2 != 0)
+    printf("%d", num2);
+  else
+    printf("0");
+  
+  return 0;
+}
